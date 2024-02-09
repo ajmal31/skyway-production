1d2c63402783de720ac06373e676a0aa704d4aba
ajmal31 <ajmalmuhammed846@gmail.com>
2024-02-09 12:26:05 +0530

    Refacotred : venture-srv 43

--- venture-srv/src/config/env.js
+++ venture-srv/src/config/env.js
@@ -5,11 +5,11 @@ export default{
 
     AWS_ACCESSKEY:process.env.AWS_ACCESSKEY,
     AWS_SECRETKEY:process.env.AWS_SECRETKEY,
-    JWT_SECRETKEY:process.env.JWT_SECRETKEY ?? "ajmal123venture-srv" ,
-    MONGO_URI:process.env.MONGO_URI ?? "mongodb+srv://ajmalmuhammed846:ajuZ3216@cluster0.uzyffxb.mongodb.net/skyway-venture-srv?retryWrites=true&w=majority" ,
+    JWT_SECRETKEY:process.env.JWT_SECRETKEY ,
+    MONGO_URI:process.env.MONGO_URI,
     APPLICATION_PORT:process.env.APPLICATION_PORT?? 3001,
-    JWT_ADMIN_SECRET_KEY:process.env.JWT_ADMIN_SECRET_KEY ?? "ajmal123admin-srv",
-    JWT_USER_SECRET_KEY:process.env.JWT_USER_SECRET_KEY ?? "ajmal123user-srv" ,
+    JWT_ADMIN_SECRET_KEY:process.env.JWT_ADMIN_SECRET_KEY ,
+    JWT_USER_SECRET_KEY:process.env.JWT_USER_SECRET_KEY  ,
 
 
 }
